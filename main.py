f6d12235ce0676ec3577fe7d4aeb3e956dc05b57
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-06-15 01:13:58 -0700

    Connected Api to the server.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests, json
 from flask import Flask
 from flask_restful import Api, Resource
 
@@ -6,14 +7,19 @@ api = Api(app)
 
 class PostResource(Resource):
     def get(self):
-        return {'data': 'Get request' }
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        url = BASE_URL + "q=" + "san francisco" + "&appid=" + "4647aaef88d6e0fbf8feeb4ce0f0cb50"
+        response = requests.get(url)
+        
+        # if response.status == 200:
+        return {'data': response.json() }
 
     def post(self):
         return {'data': 'post request' }
 
 
-api.add_resource(PostResource, '/')
-api.add_resource(PostResource, '/weather/int:id')
+api.add_resource(PostResource, '/weather')
+
 
 
 
